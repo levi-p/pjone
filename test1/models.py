677a1e99afcd4da4bad3c3eb8e427549fb019c90
levi-p <levipda@gmail.com>
2016-03-04 15:38:09 +0200

    second one

--- test1/models.py
+++ test1/models.py
@@ -1,17 +1,17 @@
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
-from django.db import models
+#from django.db import models
 
 # Create your models here.
 
-class product(models.Model):
-    name = models.CharField(max_length=50)
-    price= models.DecimalField(max_digits=20, decimal_places = 2)
-    decicription = models.TextField()
+#class product(models.Model):
+ #   name = models.CharField(max_length=50)
+  #  price= models.DecimalField(max_digits=20, decimal_places = 2)
+   # decicription = models.TextField()
 
-    class Meta:
-            
-            verbose_name_plural = 'products'
-            db_table = 'producttable'
-    def __unicode__(self):
-        return self.name
+    #class Meta:
+     #       
+      #      verbose_name_plural = 'products'
+       #     db_table = 'producttable'
+   # def __unicode__(self):
+    #    return self.name
